90a7f19697fc75c039b0d6b75423ec5008a73875
KareemMoataz02 <kareemmoataz13@gmail.com>
2025-04-11 18:31:24 +0200

    init

--- website_app/app.py
+++ website_app/app.py
@@ -108,7 +108,7 @@ def index():
           border-radius: 4px;
         }}
         input[type="submit"] {{
-          background-color: red;
+          background-color: green;
           color: white;
           padding: 10px 15px;
           border: none;
