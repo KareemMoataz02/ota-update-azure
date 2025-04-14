2add40ee48824a695f3067e630a1588b78c17bba
KareemMoataz02 <kareemmoataz13@gmail.com>
2025-04-14 21:41:46 +0200

    added mongodb

--- terraform/main.tf
+++ terraform/main.tf
@@ -229,13 +229,12 @@ resource "azurerm_linux_virtual_machine" "hmi_vm" {
   }
 
   custom_data = base64encode(templatefile("cloud-init-hmi.yaml", {
-    cosmosdb_uri        = azurerm_cosmosdb_account.mongodb.primary_mongodb_connection_string
-    cosmosdb_database   = var.mongodb_database_name
-    cosmosdb_collection = var.mongodb_collection_name
-    cosmosdb_endpoint   = azurerm_cosmosdb_account.mongodb.endpoint
-    cosmosdb_key        = azurerm_cosmosdb_account.mongodb.primary_key
-    cosmosdb_database   = var.mongodb_database_name
-    cosmosdb_collection = var.mongodb_collection_name
+    cosmosdb_uri               = azurerm_cosmosdb_account.mongodb.primary_mongodb_connection_string
+    cosmosdb_endpoint          = azurerm_cosmosdb_account.mongodb.endpoint
+    cosmosdb_key               = azurerm_cosmosdb_account.mongodb.primary_key
+    cosmosdb_database          = var.mongodb_database_name
+    cosmosdb_collection        = var.mongodb_collection_name
+    hex_storage_account_name   = var.hex_storage_account_name
+    hex_storage_container_name = azurerm_storage_container.hex_container.name
   }))
 }
-
