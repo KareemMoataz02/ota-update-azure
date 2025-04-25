eec429482a063df011a9ecae2cdbaafaa85fdc52
KareemMoataz02 <kareemmoataz13@gmail.com>
2025-04-25 20:13:52 +0300

    integ 8

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "ec34342c-37de-48d7-a62d-6d8cbf370531"
-}
+# provider "azurerm" {
+#   features {}
+#   subscription_id = "ec34342c-37de-48d7-a62d-6d8cbf370531"
+# }
 
