db28c09df407a710a40a90707f5e0afe1c986abe
KareemMoataz02 <kareemmoataz13@gmail.com>
2025-04-25 22:39:50 +0300

    infra 9

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,35 +11,29 @@ output "hmi_vm_public_ip" {
 output "env_cosmosdb_uri" {
   description = "Environment variable assignment for Cosmos DB URI"
   value       = "COSMOSDB_URI=${azurerm_cosmosdb_account.mongodb.primary_mongodb_connection_string}"
-  sensitive   = false
 }
 
 output "env_cosmosdb_database" {
   description = "Environment variable assignment for Cosmos DB database name"
   value       = "COSMOSDB_DATABASE=${var.mongodb_database_name}"
-  sensitive   = false
 }
 
 output "env_cosmosdb_collection" {
   description = "Environment variable assignment for Cosmos DB collection name"
   value       = "COSMOSDB_COLLECTION=${var.mongodb_collection_name}"
-  sensitive   = false
 }
 
 output "env_hex_storage_account_name" {
   description = "Environment variable assignment for HEX storage account name"
   value       = "HEX_STORAGE_ACCOUNT_NAME=${var.hex_storage_account_name}"
-  sensitive   = false
 }
 
 output "env_hex_storage_container_name" {
   description = "Environment variable assignment for HEX storage container name"
   value       = "HEX_STORAGE_CONTAINER_NAME=${azurerm_storage_container.hex_container.name}"
-  sensitive   = false
 }
 
 output "env_hex_storage_account_key" {
   description = "Environment variable assignment for HEX storage account key"
   value       = "HEX_STORAGE_ACCOUNT_KEY=${azurerm_storage_account.hex_storage.primary_access_key}"
-  sensitive   = false
 }
