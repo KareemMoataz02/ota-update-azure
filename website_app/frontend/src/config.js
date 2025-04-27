339cae13f2d245f68bf52bd13f1ad18234a0dc6d
KareemMoataz02 <kareemmoataz13@gmail.com>
2025-04-27 22:18:57 +0300

    Compose

--- website_app/frontend/src/config.js
+++ website_app/frontend/src/config.js
@@ -1,12 +1,10 @@
-// api.js
-const API_CONFIG = {
-  baseURL: process.env.REACT_APP_API_URL.endsWith('/')
-    ? process.env.REACT_APP_API_URL
-    : process.env.REACT_APP_API_URL + '/',
+// config.js
+export const API_CONFIG = {
+  baseURL: "/api/",          
   endpoints: {
     carTypes: "car-types",
     ecus:     "ecus",
     versions: "versions",
-    requests:"requests",
+    requests: "requests",
   },
 };
