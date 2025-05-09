b6f14b7b9e40fe4181037438019abc5404430451
KareemMoataz02 <kareemmoataz13@gmail.com>
2025-05-09 22:59:13 +0300

    test_run5

--- website_app/frontend/src/config.js
+++ website_app/frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_CONFIG = {
   // baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api/",
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "/api/",
   endpoints: {
     carTypes: "car-types",
     ecus: "ecus",
